5b4c317482c46c95036b996d0cf15483bfa4740f
ZhaoZiyu1998 <809202024qq@gmail.com>
2022-06-10 23:43:46 -0400

    Add files via upload

---
+++ CIFAR10/main.py
@@ -0,0 +1,7 @@
+import torch
+import torchvision.transforms as transforms
+import torchvision.datasets as databases
+import torch.nn
+import torch.nn.functional as F
+import torch.utils.data as td
+train_set= databases.CIFAR10('')
