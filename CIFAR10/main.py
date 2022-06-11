b4ab9db8f349d6c549bf962fb5692252ec493335
ZhaoZiyu1998 <809202024qq@gmail.com>
2022-06-10 23:44:48 -0400

    Delete main.py

--- CIFAR10/main.py
+++
@@ -1,7 +0,0 @@
-import torch
-import torchvision.transforms as transforms
-import torchvision.datasets as databases
-import torch.nn
-import torch.nn.functional as F
-import torch.utils.data as td
-train_set= databases.CIFAR10('')
